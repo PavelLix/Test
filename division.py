77774f2692b68534faccaee01bcbe7bbf9ecdf15
PavelLix <pavelbaleon@gmail.com>
2024-01-25 12:01:39 -0600

    Nueva version de division

--- division.py
+++ division.py
@@ -1,1 +1,15 @@
-print(5/2)
+a=8
+b=0
+
+try:
+    print(c/b)
+except ZeroDivisionError:
+    print("error no se puede dividir entres 0")
+except NameError:
+    print("La variable no existe")
+except:
+    print("Algo salio mal")
+
+
+
+
